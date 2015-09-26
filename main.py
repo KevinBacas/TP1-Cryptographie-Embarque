b1ecdf51ad6948362d418310007a3dbb1d224e3a
KevinBacas <bacas.kevin@hotmail.fr>
2015-09-26 00:58:23 -0700

    Fin du TP

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from utils import Boolean_function, display_truth_table, is_balanced
+from utils import boolean_function, display_truth_table, is_balanced, statistical_test, generate_keystream_with_lfsr, attaque_par_correlation
 
 if __name__ == '__main__':
-	print "Hello world!"
-	print Boolean_function(1, 0, 0)
-	display_truth_table(226, 3)
-	print is_balanced(226, 3)
+    print boolean_function(1, 0, 0)
+
+    display_truth_table(226, 3)
+
+    print is_balanced(226, 3)
+
+    print generate_keystream_with_lfsr(57, 105, 7, 20)
+
+    val1 = 0b111
+    val2 = 0b111
+    print statistical_test(val1, val2, 3)
+
+    attaque_par_correlation()
--- utils.py
+++ utils.py
@@ -1,52 +1,102 @@
-def Boolean_function(x1, x2, x3) :
-	x = x3 ^ (x2 << 1) ^ (x1 << 2)
-	return ((226 >> x) & 1)
+import random
+
+
+def boolean_function(x1, x2, x3):
+    x = x3 ^ (x2 << 1) ^ (x1 << 2)
+    return (226 >> x) & 1
+
 
 def binary_string(x, n):
-	s = ''
-	for i in range(n):
-		if ((x >> i) & 1) == 1:
-			s = "1" + s
-		else:
-			s = "0" + s
-	return s
+    s = ''
+    for i in range(n):
+        if ((x >> i) & 1) == 1:
+          s = "1" + s
+        else:
+          s = "0" + s
+    return s
+
 
 def display_truth_table(f, n):
-	for i in range(1 << n):
-		print binary_string(i, n), (f >> i) & 1
+    for i in range(1 << n):
+        print binary_string(i, n), (f >> i) & 1
+
 
 def is_balanced(f, n):
-	res = 0
-	for i in range(1 << n):
-		res = res + (1 if ((f >> i) & 1) else -1)
-	return res == 0
+    res = 0
+    for i in range(1 << n):
+        res += (1 if ((f >> i) & 1) else -1)
+    return res == 0
 
-def update_LFSR(state, poly, n):
-	sn = 0
-	for i in xrange(n-1):
-		sn = sn ^ ((poly >> (i+1))&1) & ((state >> (n-1-i))&1)
-	return ((state >> 1) ^ (sn << (n-1))), (state & 1)
 
-def generate_keystream_with_LFSR(state, poly, n, l):
-	res = 0
-	tmp_state = state
-	for i in xrange(l):
-		tmp_state, lower_bit = update_LFSR(tmp_state, poly, n)
-		res = (lower_bit << i) ^ (res)
-	return res
+def update_lfsr(state, poly, n):
+    sn = 0
+    for i in xrange(n-1):
+        sn ^= ((poly >> (i+1)) & 1) & ((state >> (n-1-i)) & 1)
+    return ((state >> 1) ^ (sn << n-1)), (state & 1)
 
-def Generator(cle, l):
-	cle1 = cle & 0b1111111111 # 10 bits
-	cle2 = (cle >> 10) & 0b11111111111 # 11 bits
-	cle3 = (cle >> 21) # 12 bits
-	poly1 = (1 << 10) ^ (1 << 3) ^ 1
-	poly2 = (1 << 11) ^ (1 << 2) ^ 1
-	poly3 = (1 << 12) ^ (1 << 6) ^ (1 << 4) ^ (1 << 1) ^ 1
-	x1 = generate_keystream_with_LFSR(cle1, poly1, 10, l)
-	x2 = generate_keystream_with_LFSR(cle2, poly2, 11, l)
-	x3 = generate_keystream_with_LFSR(cle3, poly3, 12, l)
-	res = 0
-	for i in xrange(l):
-		res = (res << 1) ^ Boolean_function((x1 >> i)&1, (x2 >> i)&1, (x3 >> i)&1)
-	return res
-		
+
+def generate_keystream_with_lfsr(state, poly, n, l):
+    res = 0
+    tmp_state = state
+    for i in xrange(l):
+        tmp_state, lower_bit = update_lfsr(tmp_state, poly, n)
+        res = (lower_bit << i) ^ res
+    return res
+
+
+def generator(cle, l):
+    cle1 = cle & 0b1111111111  			# 10 bits
+    cle2 = (cle >> 10) & 0b11111111111  # 11 bits
+    cle3 = (cle >> 21)  				# 12 bits
+    poly1 = (1 << 10) ^ (1 << 3) ^ 1
+    poly2 = (1 << 11) ^ (1 << 2) ^ 1
+    poly3 = (1 << 12) ^ (1 << 6) ^ (1 << 4) ^ (1 << 1) ^ 1
+    x1 = generate_keystream_with_lfsr(cle1, poly1, 10, l)
+    x2 = generate_keystream_with_lfsr(cle2, poly2, 11, l)
+    x3 = generate_keystream_with_lfsr(cle3, poly3, 12, l)
+    res = 0
+    for i in xrange(l):
+        res = (res << 1) ^ boolean_function((x1 >> i) & 1, (x2 >> i) & 1, (x3 >> i) & 1)
+    return res
+
+
+def statistical_test(s1, s2, l):
+    count = 0
+    for i in xrange(l):
+        if ((s1 >> i) & 1) == ((s2 >> i) & 1):
+            count += 1
+    return count > (l * 3 / 4)
+
+
+def attaque_par_correlation():
+    cle_random = random.randint(0, 1 << 33)
+    cle1 = cle_random & 0b1111111111
+    cle2 = (cle_random >> 10) & 0b11111111111
+    cle3 = (cle_random >> 21)
+    print "cle1 :", cle1
+    print "cle2 :", cle2
+    print "cle3 :", cle3
+    cle_300 = generator(cle_random, 300)
+    poly1 = (1 << 10) ^ (1 << 3) ^ 1
+    poly2 = (1 << 11) ^ (1 << 2) ^ 1
+    poly3 = (1 << 12) ^ (1 << 6) ^ (1 << 4) ^ (1 << 1) ^ 1
+
+    # Recherche LFSR 1
+    LFSR1 = 0
+    foundLFSR1 = False
+    while not foundLFSR1 and LFSR1 < (1 << 11):
+        if statistical_test(cle_300, generate_keystream_with_lfsr(LFSR1, poly1, 10, 300), 300):
+            foundLFSR1 = True
+        else:
+            LFSR1 += 1
+    print statistical_test(cle_300, generate_keystream_with_lfsr(LFSR1, poly1, 10, 300), 300), LFSR1
+
+    # Recherche LFSR 3
+    LFSR3 = 0
+    foundLFSR3 = False
+    while not foundLFSR3 and LFSR3 < (1 << 13):
+        if statistical_test(cle_300, generate_keystream_with_lfsr(LFSR3, poly3, 10, 300), 300):
+            foundLFSR3 = True
+        else:
+            foundLFSR3 += 1
+    print statistical_test(cle_300, generate_keystream_with_lfsr(LFSR3, poly3, 10, 300), 300), LFSR3
