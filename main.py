ca08bf13cf0902ebd6d7187e4a5bcb68edd72f14
Bacas Kevin <bacas@ecole.ensicaen.fr>
2015-09-28 08:56:51 +0200

    Added some utils functions

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
+from utils import Boolean_function
+
 if __name__ == '__main__':
 	print "Hello world!"
+	print Boolean_function(1, 0, 0)
---
+++ utils.py
@@ -0,0 +1,14 @@
+def Boolean_function(x1, x2, x3) :
+	x = x3 ^ (x2 << 1) ^ (x1 << 2)
+	return ((226 >> x) & 1)
+
+def binary_string(x, n):
+	s = ''
+	for i in range(n):
+		if ((x >> i) & 1) == 1: s = "1" + s
+		else: s = "0" + s
+	return s
+
+def display_truth_table(f, n):
+	for i in range(1 << n):
+		print binary_string(i, n), (f >> i) & 1
