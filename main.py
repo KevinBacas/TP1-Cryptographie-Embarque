f88efa3b5a53ca73d4ef20f427e3847d5232fb75
Bacas Kevin <bacas@ecole.ensicaen.fr>
2015-09-28 09:15:28 +0200

    Added is_balanced(f, n)

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from utils import Boolean_function
+from utils import Boolean_function, display_truth_table, is_balanced
 
 if __name__ == '__main__':
 	print "Hello world!"
 	print Boolean_function(1, 0, 0)
+	display_truth_table(226, 3)
+	print is_balanced(226, 3)
--- utils.py
+++ utils.py
@@ -5,10 +5,18 @@ def Boolean_function(x1, x2, x3) :
 def binary_string(x, n):
 	s = ''
 	for i in range(n):
-		if ((x >> i) & 1) == 1: s = "1" + s
-		else: s = "0" + s
+		if ((x >> i) & 1) == 1:
+			s = "1" + s
+		else:
+			s = "0" + s
 	return s
 
 def display_truth_table(f, n):
 	for i in range(1 << n):
 		print binary_string(i, n), (f >> i) & 1
+
+def is_balanced(f, n):
+	res = 0
+	for i in range(1 << n):
+		res = res + (f >> i) & 1
+	return res == 0
